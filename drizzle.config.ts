d2cf8aa666f3fc6fdddd43d61a05150d24b76bc0
Jesse Quartey <58194213+jessequartey@users.noreply.github.com>
2025-09-17 20:18:18 +0000

    auth config done.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
 });

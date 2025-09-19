644200c023a38e1175f947e1a4e2feda4b92e3f6
Jesse Quartey <58194213+jessequartey@users.noreply.github.com>
2025-09-19 17:45:59 +0000

    fix db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,13 +1,5 @@
-/** biome-ignore-all lint/performance/noNamespaceImport: <neccessary> */
-import { neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
-import * as schema from "@/server/db/schema";
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from '@/server/db/schema';
 
-neonConfig.webSocketConstructor = ws;
+const db = drizzle(process.env.DATABASE_URL || "", {schema});
 
-export const db = drizzle({
-  connection: process.env.DATABASE_URL || "",
-  ws,
-  schema,
-});

ade55f7c8da38bebb2c4e5f1df66ccb38dcf6e26
Jesse Quartey <58194213+jessequartey@users.noreply.github.com>
2025-09-19 17:46:56 +0000

    update

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '@/server/db/schema';
 
-const db = drizzle(process.env.DATABASE_URL || "", {schema});
+export const db = drizzle(process.env.DATABASE_URL || "", {schema});
 
